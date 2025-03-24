6481aa635fdc747f8c94dee7f1f16e9b1335cda9
Prasad7218 <yerawarprasad18@gmail.com>
2025-03-24 10:58:38 +0530

    loader added and routes set up done

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import Loader from '../components/Loader';
+
+const Home = () => {
+  return (
+    <>
+    <div>Home</div>
+    {/* <Loader/> */}
+    </>
+  )
+}
+
+export default Home;
